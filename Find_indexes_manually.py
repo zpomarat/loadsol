0de7da9f9b17d4863800a2eecb21ff3716c19560
Zoé Pomarat <zoepom.zp@gmail.com>
2024-02-01 11:50:35 +0100

    Create TimeSeries.

--- Find_indexes_manually.py
+++ Find_indexes_manually.py
@@ -8,12 +8,9 @@
 # To find the indexes of these events, use the kinetics toolkit and the TimeSeries.
 
 from os import getcwd, listdir
-
-
 from DataLoadsol import DataLoadsol
 from DataForceplates import DataForceplates
 import kineticstoolkit as ktk
-
 import matplotlib.pyplot as plt
 ktk.import_extensions()
 ktk.change_defaults()
@@ -53,17 +50,6 @@ poussee_1_ls = DataLoadsol(path=working_directory + txt_files[4], frequency=200)
 poussee_2_ls = DataLoadsol(path=working_directory + txt_files[5], frequency=200)
 poussee_3_ls = DataLoadsol(path=working_directory + txt_files[6], frequency=200)
 
-# List of DataLoadsol objects
-data_ls_list = [
-    "poids_ls",
-    "pointe_1_ls",
-    "pointe_3_ls",
-    "pointe_5_ls",
-    "poussee_1_ls",
-    "poussee_2_ls",
-    "poussee_3_ls",
-]
-
 # Define DataForceplates objects
 poids_fp = DataForceplates(path=working_directory + c3d_files[0], frequency=1000)
 pointe_1_fp = DataForceplates(path=working_directory + c3d_files[1], frequency=1000)
@@ -72,17 +58,6 @@ pointe_5_fp = DataForceplates(path=working_directory + c3d_files[3], frequency=1
 poussee_1_fp = DataForceplates(path=working_directory + c3d_files[4], frequency=1000)
 poussee_2_fp = DataForceplates(path=working_directory + c3d_files[5], frequency=1000)
 poussee_3_fp = DataForceplates(path=working_directory + c3d_files[6], frequency=1000)
-
-# List of DataLoadsol objects
-data_fp_list = [
-    "poids_fp",
-    "pointe_1_fp",
-    "pointe_3_fp",
-    "pointe_5_fp",
-    "poussee_1_fp",
-    "poussee_2_fp",
-    "poussee_3_fp",
-]
 
 # Convert txt files into csv files  ## TODO: csv conversion doesn't work correctly
 if "test_poids.csv" in listdir(working_directory):
@@ -99,12 +74,91 @@ else:
     print("txt files converted into csv files.")
 
 ## Get variables of interest for finding indexes
-# Acceleration of one insole (both are synchronised)
-poids_raw_l_acc = poids_ls.get_raw_data(insole_side="LEFT", data_type="ACC")
-pointe_1_raw_l_acc = pointe_1_ls.get_raw_data(insole_side="LEFT", data_type="ACC")
-pointe_3_raw_l_acc = pointe_3_ls.get_raw_data(insole_side="LEFT", data_type="ACC")
-pointe_5_raw_l_acc = pointe_5_ls.get_raw_data(insole_side="LEFT", data_type="ACC")
+# Insoles time
+poids_pp_time_ls = poids_ls.get_pre_processed_time()   
+pointe_1_pp_time_ls = pointe_1_ls.get_pre_processed_time()
+pointe_3_pp_time_ls = pointe_3_ls.get_pre_processed_time()
+pointe_5_pp_time_ls = pointe_5_ls.get_pre_processed_time()
+poussee_1_pp_time_ls = poussee_1_ls.get_pre_processed_time()
+poussee_2_pp_time_ls = poussee_2_ls.get_pre_processed_time()
+poussee_3_pp_time_ls = poussee_3_ls.get_pre_processed_time()
 
-for val in [poids_raw_l_acc,pointe_1_raw_l_acc,pointe_3_raw_l_acc, pointe_5_raw_l_acc]:
-    plt.plot(val)
-plt.show()
+# Acceleration of one insole (both are synchronised)
+poids_pp_l_acc = poids_ls.get_pre_processed_data(insole_side="LEFT", data_type="ACC")
+pointe_1_pp_l_acc = pointe_1_ls.get_pre_processed_data(insole_side="LEFT", data_type="ACC")
+pointe_3_pp_l_acc = pointe_3_ls.get_pre_processed_data(insole_side="LEFT", data_type="ACC")
+pointe_5_pp_l_acc = pointe_5_ls.get_pre_processed_data(insole_side="LEFT", data_type="ACC")
+poussee_1_pp_l_acc = poussee_1_ls.get_pre_processed_data(insole_side="LEFT", data_type="ACC")
+poussee_2_pp_l_acc = poussee_2_ls.get_pre_processed_data(insole_side="LEFT", data_type="ACC")
+poussee_3_pp_l_acc = poussee_3_ls.get_pre_processed_data(insole_side="LEFT", data_type="ACC")
+
+# Forceceplate force in z direction for the forceplates 1 and 2
+poids_pp_fp2 = poids_fp.get_pre_processed_data(forceplate_number=2)
+pointe_1_pp_fp2 = pointe_1_fp.get_pre_processed_data(forceplate_number=2)
+pointe_3_pp_fp2 = pointe_3_fp.get_pre_processed_data(forceplate_number=2)
+pointe_5_pp_fp2 = pointe_5_fp.get_pre_processed_data(forceplate_number=2)
+poussee_1_pp_fp2 = poussee_1_fp.get_pre_processed_data(forceplate_number=2)
+poussee_2_pp_fp2 = poussee_2_fp.get_pre_processed_data(forceplate_number=2)
+poussee_3_pp_fp2 = poussee_3_fp.get_pre_processed_data(forceplate_number=2)
+
+# Forceplates time
+poids_pp_time_fp = poids_fp.get_pre_processed_time()   
+pointe_1_pp_time_fp = pointe_1_fp.get_pre_processed_time()
+pointe_3_pp_time_fp = pointe_3_fp.get_pre_processed_time()
+pointe_5_pp_time_fp = pointe_5_fp.get_pre_processed_time()
+poussee_1_pp_time_fp = poussee_1_fp.get_pre_processed_time()
+poussee_2_pp_time_fp = poussee_2_fp.get_pre_processed_time()
+poussee_3_pp_time_fp = poussee_3_fp.get_pre_processed_time()
+
+## Create TimeSeries
+poids_ls_ts = ktk.TimeSeries()
+poids_fp_ts = ktk.TimeSeries()
+pointe_1_ls_ts = ktk.TimeSeries()
+pointe_1_fp_ts = ktk.TimeSeries()
+pointe_3_ls_ts = ktk.TimeSeries()
+pointe_3_fp_ts = ktk.TimeSeries()
+pointe_5_ls_ts = ktk.TimeSeries()
+pointe_5_fp_ts = ktk.TimeSeries()
+poussee_1_ls_ts = ktk.TimeSeries()
+poussee_1_fp_ts = ktk.TimeSeries()
+poussee_2_ls_ts = ktk.TimeSeries()
+poussee_2_fp_ts = ktk.TimeSeries()
+poussee_3_ls_ts = ktk.TimeSeries()
+poussee_3_fp_ts = ktk.TimeSeries()
+
+## Add data to the TimeSeries
+poids_ls_ts.time = poids_pp_time_ls
+poids_fp_ts.time = poids_pp_time_fp
+pointe_1_ls_ts.time = pointe_1_pp_time_ls
+pointe_1_fp_ts.time = pointe_1_pp_time_fp
+pointe_3_ls_ts.time = pointe_3_pp_time_ls
+pointe_3_fp_ts.time = pointe_3_pp_time_fp
+pointe_5_ls_ts.time = pointe_5_pp_time_ls
+pointe_5_fp_ts.time = pointe_5_pp_time_fp
+poussee_1_ls_ts.time = poussee_1_pp_time_ls
+poussee_1_fp_ts.time = poussee_1_pp_time_fp
+poussee_2_ls_ts.time = poussee_2_pp_time_ls
+poussee_2_fp_ts.time = poussee_2_pp_time_fp
+poussee_3_ls_ts.time = poussee_3_pp_time_ls
+poussee_3_fp_ts.time = poussee_3_pp_time_fp
+
+poids_ls_ts.data = poids_pp_l_acc
+poids_fp_ts.data = poids_pp_time_fp
+pointe_1_ls_ts.data = pointe_1_pp_l_acc
+pointe_1_fp_ts.data = pointe_1_pp_fp2
+pointe_3_ls_ts.data = pointe_3_pp_l_acc
+pointe_3_fp_ts.data = pointe_3_pp_fp2
+pointe_5_ls_ts.data = pointe_5_pp_l_acc
+pointe_5_fp_ts.data = pointe_5_pp_fp2
+poussee_1_ls_ts.data = poussee_1_pp_l_acc
+poussee_1_fp_ts.data = poussee_1_pp_fp2
+poussee_2_ls_ts.data = poussee_2_pp_l_acc
+poussee_2_fp_ts.data = poussee_2_pp_fp2
+poussee_3_ls_ts.data = poussee_3_pp_l_acc
+poussee_3_fp_ts.data = poussee_3_pp_fp2
+
+
+
+# for val in [poids_raw_l_acc,pointe_1_raw_l_acc,pointe_3_raw_l_acc, pointe_5_raw_l_acc]:
+#     plt.plot(val)
+# plt.show()
