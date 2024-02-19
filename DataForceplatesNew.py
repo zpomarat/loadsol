86e4bbf29a89e145a845ed8adb7d9212065570bf
Zoé Pomarat <zoepom.zp@gmail.com>
2024-02-19 11:01:04 +0100

    Restructure DataForceplates. Add c3d_reader.

---
+++ DataForceplatesNew.py
@@ -0,0 +1,123 @@
+import Data
+from os import getcwd
+from datetime import datetime
+import c3d
+import numpy as np
+import matplotlib.pyplot as plt
+
+class DataForceplates(Data.Data):
+    def __init__(self, path: str, frequency:int):
+        super().__init__(path, frequency)
+        self.path = path
+        self.file_name = (self.path.split("\\"))[-1].split(".")[0]
+        self.frequency = frequency
+        self.raw_data = None
+        self.timestamp = None
+        self.pre_processed_data = None
+        self.resampled_data = None
+        self.filtered_data = None
+
+    def c3d_reader_forceplates(self):
+        """Reads a c3d file and extracts the raw analog data.
+
+        Returns:
+            raw_data (np.ndarray): raw analog data [ix30]
+        """
+        
+        ## Extract timestamp
+        # Path of the xcp file containing the timestamp
+        file_path = self.path[:-3] + "xcp"
+
+        # Read the line line containing the timestamp in the text file
+        with open(file_path) as file:
+            lines = file.readlines()
+
+        # Line containing the timestamp
+        line = lines[7]
+
+        # Split the line to extract the part containing the timestamp
+        txt = line.split('START_TIME="')
+
+        # Extract the timestamp as a str
+        str_timestamp = txt[1][:-7]
+
+        # Specify the format of the timestamp (Year-Month-Day Hour:Minute:Second.Microsecond)
+        format = '%Y-%m-%d %H:%M:%S.%f'
+
+        # Define the timestamp
+        timestamp = datetime.strptime(str_timestamp,format)
+        self.timestamp = timestamp
+
+        ## Read the c3d file
+        reader = c3d.Reader(open(self.path,'rb'))
+
+        # Extract analog data (raw data + others parameters)
+        data_analog = []
+    
+        for analog in reader.read_frames():
+            data_analog.append(analog)
+
+        # Create an array containing only the raw analog data
+        raw_data = []
+        
+        for i in range(len(data_analog)):
+            if i==0:
+                raw_data = data_analog[i][2].T
+            else:  
+                raw_data = np.concatenate((raw_data,data_analog[i][2].T),axis=0)
+        
+        ## Extract time
+        time = np.arange(0,len(raw_data)/self.frequency,1/self.frequency)
+
+        ## Extract raw data
+        raw_data_f1 = raw_data[:,0:3]
+        raw_data_f2 = raw_data[:,6:9]
+        raw_data_f3 = raw_data[:,12:15]
+        raw_data_f4 = raw_data[:,18:21]
+        raw_data_f5 = raw_data[:,24:27]
+
+        ## Fill the dictionnary with raw data
+        self.raw_data = {
+                "time": time,
+                "fx1": raw_data_f1[:,0],
+                "fy1": raw_data_f1[:,1],
+                "fz1": raw_data_f1[:,2],
+                "fx2": raw_data_f2[:,0],
+                "fy2": raw_data_f2[:,1],
+                "fz2": raw_data_f2[:,2],
+                "fx3": raw_data_f3[:,0],
+                "fy3": raw_data_f3[:,1],
+                "fz3": raw_data_f3[:,2],
+                "fx4": raw_data_f4[:,0],
+                "fy4": raw_data_f4[:,1],
+                "fz4": raw_data_f4[:,2]}
+        try:
+            self.raw_data["fx5"] = raw_data_f5[:,0]
+        except:
+            self.raw_data["fx5"] = []
+        try:
+            self.raw_data["fy5"] = raw_data_f5[:,0]
+        except:
+            self.raw_data["fy5"] = []
+        try:
+            self.raw_data["fz5"] = raw_data_f5[:,0]
+        except:
+            self.raw_data["fz5"] = []
+                
+
+            
+
+
+if __name__ == "__main__":
+    curr_path = getcwd()
+
+    test = DataForceplates(curr_path + "\\tests_09_02_24\\data\\poussee_5_L.c3d", frequency=1000)
+    test.c3d_reader_forceplates()
+    plt.plot(test.raw_data['time'],test.raw_data['fx1'],label='fx1')
+    plt.plot(test.raw_data['time'],test.raw_data['fy1'],label='fy1')
+    plt.plot(test.raw_data['time'],test.raw_data['fz1'],label='fz1')
+    plt.plot(test.raw_data['time'],test.raw_data['fx2'],label='fx2')
+    plt.plot(test.raw_data['time'],test.raw_data['fy2'],label='fy2')
+    plt.plot(test.raw_data['time'],test.raw_data['fz2'],label='fz2')
+    plt.legend()
+    plt.show()
