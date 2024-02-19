d1b30cac29c3e3d7bcb21454dc2b72ac1ed35813
Zoé Pomarat <zoepom.zp@gmail.com>
2024-02-19 11:20:19 +0100

    Add pre_process_data.

--- DataForceplatesNew.py
+++ DataForceplatesNew.py
@@ -3,6 +3,7 @@ from os import getcwd
 from datetime import datetime
 import c3d
 import numpy as np
+from copy import deepcopy
 import matplotlib.pyplot as plt
 
 class DataForceplates(Data.Data):
@@ -18,10 +19,7 @@ class DataForceplates(Data.Data):
         self.filtered_data = None
 
     def c3d_reader_forceplates(self):
-        """Reads a c3d file and extracts the raw analog data.
-
-        Returns:
-            raw_data (np.ndarray): raw analog data [ix30]
+        """Reads a c3d file and extracts the raw analog data and time.
         """
         
         ## Extract timestamp
@@ -103,6 +101,95 @@ class DataForceplates(Data.Data):
             self.raw_data["fz5"] = raw_data_f5[:,0]
         except:
             self.raw_data["fz5"] = []
+
+    def pre_process_data(self):
+        """Inverses the orientation of the forceplates and set values to zero.
+        """
+
+        if self.raw_data is None:
+            self.c3d_reader_forceplates()
+
+        # Initialise pre processed data
+        self.pre_processed_data = deepcopy(self.raw_data)
+
+        # Change orientation
+        self.pre_processed_data["fx1"] = -self.pre_processed_data["fx1"]
+        self.pre_processed_data["fy1"] = -self.pre_processed_data["fy1"]
+        self.pre_processed_data["fz1"] = -self.pre_processed_data["fz1"]
+
+        self.pre_processed_data["fx2"] = -self.pre_processed_data["fx2"]
+        self.pre_processed_data["fy2"] = -self.pre_processed_data["fy2"]
+        self.pre_processed_data["fz2"] = -self.pre_processed_data["fz2"]
+
+        self.pre_processed_data["fx3"] = -self.pre_processed_data["fx3"]
+        self.pre_processed_data["fy3"] = -self.pre_processed_data["fy3"]
+        self.pre_processed_data["fz3"] = -self.pre_processed_data["fz3"]
+
+        self.pre_processed_data["fx4"] = -self.pre_processed_data["fx4"]
+        self.pre_processed_data["fy4"] = -self.pre_processed_data["fy4"]
+        self.pre_processed_data["fz4"] = -self.pre_processed_data["fz4"]
+
+        try:
+            self.pre_processed_data["fx5"] = -self.pre_processed_data["fx5"]
+            self.pre_processed_data["fy5"] = -self.pre_processed_data["fy5"]
+            self.pre_processed_data["fz5"] = -self.pre_processed_data["fz5"]
+        except:
+            self.pre_processed_data["fx5"] = []
+            self.pre_processed_data["fy5"] = []
+            self.pre_processed_data["fz5"] = []
+
+        # Set values to zero
+        # Mean values of force on the 1000 first samples
+        mean_fx1 = np.mean(self.pre_processed_data["fx1"][0:1000])
+        mean_fy1 = np.mean(self.pre_processed_data["fy1"][0:1000])
+        mean_fz1 = np.mean(self.pre_processed_data["fz1"][0:1000])
+
+        mean_fx2 = np.mean(self.pre_processed_data["fx2"][0:1000])
+        mean_fy2 = np.mean(self.pre_processed_data["fy2"][0:1000])
+        mean_fz2 = np.mean(self.pre_processed_data["fz2"][0:1000])
+        
+        mean_fx3 = np.mean(self.pre_processed_data["fx3"][0:1000])
+        mean_fy3 = np.mean(self.pre_processed_data["fy3"][0:1000])
+        mean_fz3 = np.mean(self.pre_processed_data["fz3"][0:1000])
+        
+        mean_fx4 = np.mean(self.pre_processed_data["fx4"][0:1000])
+        mean_fy4 = np.mean(self.pre_processed_data["fy4"][0:1000])
+        mean_fz4 = np.mean(self.pre_processed_data["fz4"][0:1000])
+
+        try: 
+            mean_fx5 = np.mean(self.pre_processed_data["fx5"][0:1000])
+            mean_fy5 = np.mean(self.pre_processed_data["fy5"][0:1000])
+            mean_fz5 = np.mean(self.pre_processed_data["fz5"][0:1000])
+        except:
+            mean_fx5 = []
+            mean_fy5 = []
+            mean_fz5 = []
+        
+        # Substract the mean value to the data
+        self.pre_processed_data["fx1"][:] -= mean_fx1
+        self.pre_processed_data["fy1"][:] -= mean_fy1
+        self.pre_processed_data["fz1"][:] -= mean_fz1
+
+        self.pre_processed_data["fx2"][:] -= mean_fx2
+        self.pre_processed_data["fy2"][:] -= mean_fy2
+        self.pre_processed_data["fz2"][:] -= mean_fz2
+
+        self.pre_processed_data["fx3"][:] -= mean_fx3
+        self.pre_processed_data["fy3"][:] -= mean_fy3
+        self.pre_processed_data["fz3"][:] -= mean_fz3
+
+        self.pre_processed_data["fx4"][:] -= mean_fx4
+        self.pre_processed_data["fy4"][:] -= mean_fy4
+        self.pre_processed_data["fz4"][:] -= mean_fz4
+
+        try:
+            self.pre_processed_data["fx5"][:] -= mean_fx5
+            self.pre_processed_data["fy5"][:] -= mean_fy5
+            self.pre_processed_data["fz5"][:] -= mean_fz5
+        except:
+            self.pre_processed_data["fx5"] = []
+            self.pre_processed_data["fy5"] = []
+            self.pre_processed_data["fz5"] = []
                 
 
             
@@ -112,6 +199,7 @@ if __name__ == "__main__":
     curr_path = getcwd()
 
     test = DataForceplates(curr_path + "\\tests_09_02_24\\data\\poussee_5_L.c3d", frequency=1000)
+
     test.c3d_reader_forceplates()
     plt.plot(test.raw_data['time'],test.raw_data['fx1'],label='fx1')
     plt.plot(test.raw_data['time'],test.raw_data['fy1'],label='fy1')
@@ -120,4 +208,17 @@ if __name__ == "__main__":
     plt.plot(test.raw_data['time'],test.raw_data['fy2'],label='fy2')
     plt.plot(test.raw_data['time'],test.raw_data['fz2'],label='fz2')
     plt.legend()
+    plt.title("Raw data")
+    plt.figure()
+
+    test.pre_process_data()
+    plt.plot(test.pre_processed_data['time'],test.pre_processed_data['fx1'],label='fx1')
+    plt.plot(test.pre_processed_data['time'],test.pre_processed_data['fy1'],label='fy1')
+    plt.plot(test.pre_processed_data['time'],test.pre_processed_data['fz1'],label='fz1')
+    plt.plot(test.pre_processed_data['time'],test.pre_processed_data['fx2'],label='fx2')
+    plt.plot(test.pre_processed_data['time'],test.pre_processed_data['fy2'],label='fy2')
+    plt.plot(test.pre_processed_data['time'],test.pre_processed_data['fz2'],label='fz2')
+    plt.legend()
+    plt.title("Pre-processed data")
     plt.show()
+
