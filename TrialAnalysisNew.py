c9e9050cdd95585c0f2f12f167cd685cc66df784
Zoé Pomarat <zoepom.zp@gmail.com>
2024-02-19 16:22:32 +0100

    Change initialisation (downsampling + manual synchronisation.

--- TrialAnalysisNew.py
+++ TrialAnalysisNew.py
@@ -4,23 +4,54 @@ from DataLoadsolNew import DataLoadsol
 from DataForceplatesNew import DataForceplates
 import matplotlib.pyplot as plt
 
+
 class TrialAnalysis:
     def __init__(
         self,
         DataLoadSol: DataLoadsol,
-        DataForceplate: DataForceplates,
-        final_frequency: int        
+        DataForcePlates: DataForceplates,
+        sync_index_loadsol: int,
+        sync_index_forceplates: int,
+        final_frequency: int,
     ):
         self.data_loadsol = deepcopy(DataLoadSol)
-        self.data_forceplate = deepcopy(DataForceplate)
+        self.data_forceplates = deepcopy(DataForcePlates)
         self.data_loadsol_sync = None
-        self.data_forceplate_sync = None
+        self.data_forceplates_sync = None
 
+        ## Synchronise manually signals
+        # Initialise synchronised data
         if DataLoadSol is not None:
             self.data_loadsol.downsample(final_frequency)
-        if DataForceplates is not None:
-            self.data_forceplate.downsample(final_frequency)
+        if DataForcePlates is not None:
+            self.data_forceplates.downsample(final_frequency)
 
+        self.data_loadsol_sync = deepcopy(self.data_loadsol.downsampled_data)
+        self.data_forceplates_sync = deepcopy(self.data_forceplates.downsampled_data)
+
+        # Start signals at the start index
+        for key_ls in self.data_loadsol_sync.keys():
+            self.data_loadsol_sync[key_ls] = self.data_loadsol_sync[key_ls][
+                sync_index_loadsol:
+            ]
+
+        for key_fp in self.data_forceplates_sync.keys():
+            self.data_forceplates_sync[key_fp] = self.data_forceplates_sync[key_fp][
+                sync_index_forceplates:
+            ]
+       
+        # Cut signals to the dimension of the smallest one
+        min_len = min(len(self.data_loadsol_sync["time"]), len(self.data_forceplates_sync["time"]))
+
+        for key_ls in self.data_loadsol_sync.keys():
+            self.data_loadsol_sync[key_ls] = self.data_loadsol_sync[key_ls][:min_len]
+
+        for key_fp in self.data_forceplates_sync.keys():
+            self.data_forceplates_sync[key_fp] = self.data_forceplates_sync[key_fp][:min_len]
+
+        # Start time to zero
+        self.data_loadsol_sync["time"] -= self.data_loadsol_sync["time"][0]
+        self.data_forceplates_sync["time"] -= self.data_forceplates_sync["time"][0]
 
 
 if __name__ == "__main__":
@@ -37,15 +68,37 @@ if __name__ == "__main__":
     )
 
     Trial = TrialAnalysis(
-        DataLoadSol=poussee_1_L_ls,
-        DataForceplate=poussee_1_L_fp,
-        final_frequency = 200
+        DataLoadSol=poussee_1_L_ls, DataForcePlates=poussee_1_L_fp, final_frequency=200, sync_index_loadsol=2345, sync_index_forceplates=int(12092/5)
     )
 
-    # Initialise data
-    plt.plot(Trial.data_loadsol.downsampled_data["time"], Trial.data_loadsol.downsampled_data["f_total_l"],'-o',label="f total left")
-    plt.plot(Trial.data_forceplate.downsampled_data["time"], Trial.data_forceplate.downsampled_data["fz1"],'-x',label="fz1")
+    # Initialise data (synchronised signals)
+    plt.plot(Trial.data_loadsol_sync["time"],Trial.data_loadsol_sync['f_total_l'],"-o",label="f total left")
+    plt.plot(Trial.data_forceplates_sync["time"],Trial.data_forceplates_sync["fz1"],"-x",label="fz1")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Force (N)")
     plt.legend()
-    plt.title("Total force of the insole vs vertical force of the forceplate for the left foot")
+    plt.title("Synchronised  and resampled data")
     plt.show()
 
+
+    # plt.plot(
+    #     Trial.data_loadsol.downsampled_data["time"],
+    #     Trial.data_loadsol.downsampled_data["f_total_l"],
+    #     "-o",
+    #     label="f total left",
+    # )
+    # plt.plot(
+    #     Trial.data_forceplates.downsampled_data["time"],
+    #     Trial.data_forceplates.downsampled_data["fz1"],
+    #     "-x",
+    #     label="fz1",
+    # )
+    # plt.legend()
+    # plt.title(
+    #     "Total force of the insole vs vertical force of the forceplate for the left foot"
+    # )
+    # plt.show()
+
+    
+
+    print("vh")
