4b34f00216f367f9fad6e2782cf573025307e9e8
zpomarat <zoepom.zp@gmail.com>
2024-03-04 13:36:30 +0100

    Small changes in the possibility to analyse pre-processed or filtered data. Add ccompare_forces_imu.

--- TrialAnalysisNew.py
+++ TrialAnalysisNew.py
@@ -14,7 +14,7 @@ class TrialAnalysis:
         sync_index_loadsol: int,
         sync_index_forceplates: int,
         final_frequency: int,
-        data_state = "raw",
+        data_state = "pre_processed",
         order = 4,
         fcut = 20
     ):
@@ -22,50 +22,50 @@ class TrialAnalysis:
         self.data_forceplates = deepcopy(DataForcePlates)
         self.data_loadsol_sync = None
         self.data_forceplates_sync = None
+        self.data_state = data_state
 
         ## Synchronise manually signals
         # Initialise synchronised data
-        if data_state is "raw":
+        if data_state == "pre_processed":
             if DataLoadSol is not None:
                 self.data_loadsol.downsample(final_frequency)
+                self.data_loadsol_sync = deepcopy(self.data_loadsol.downsampled_data)
             if DataForcePlates is not None:
                 self.data_forceplates.downsample(final_frequency)
-
-            self.data_loadsol_sync = deepcopy(self.data_loadsol.downsampled_data)
-            self.data_forceplates_sync = deepcopy(self.data_forceplates.downsampled_data)
-
-        elif data_state is "filtered":
+                self.data_forceplates_sync = deepcopy(self.data_forceplates.downsampled_data)
+            
+        elif data_state == "filtered":
             if DataLoadSol is not None:
                 self.data_loadsol.filter(fs = final_frequency, order = order, fcut = fcut)
+                self.data_loadsol_sync = deepcopy(self.data_loadsol.filtered_data)
             if DataForcePlates is not None:
                 self.data_forceplates.filter(fs = final_frequency, order = order, fcut = fcut)
-
-            self.data_loadsol_sync = deepcopy(self.data_loadsol.filtered_data)
-            self.data_forceplates_sync = deepcopy(self.data_forceplates.filtered_data)
-
+                self.data_forceplates_sync = deepcopy(self.data_forceplates.filtered_data)
+            
         # Start signals at the start index
-        for key_ls in self.data_loadsol_sync.keys():
-            self.data_loadsol_sync[key_ls] = self.data_loadsol_sync[key_ls][
-                sync_index_loadsol:
-            ]
+        if DataLoadSol is not None and DataForcePlates is not None:
+            for key_ls in self.data_loadsol_sync.keys():
+                self.data_loadsol_sync[key_ls] = self.data_loadsol_sync[key_ls][
+                    sync_index_loadsol:
+                ]
 
-        for key_fp in self.data_forceplates_sync.keys():
-            self.data_forceplates_sync[key_fp] = self.data_forceplates_sync[key_fp][
-                sync_index_forceplates:
-            ]
+            for key_fp in self.data_forceplates_sync.keys():
+                self.data_forceplates_sync[key_fp] = self.data_forceplates_sync[key_fp][
+                    sync_index_forceplates:
+                ]
        
-        # Cut signals to the dimension of the smallest one
-        min_len = min(len(self.data_loadsol_sync["time"]), len(self.data_forceplates_sync["time"]))
+            # Cut signals to the dimension of the smallest one
+            min_len = min(len(self.data_loadsol_sync["time"]), len(self.data_forceplates_sync["time"]))
 
-        for key_ls in self.data_loadsol_sync.keys():
-            self.data_loadsol_sync[key_ls] = self.data_loadsol_sync[key_ls][:min_len]
+            for key_ls in self.data_loadsol_sync.keys():
+                self.data_loadsol_sync[key_ls] = self.data_loadsol_sync[key_ls][:min_len]
 
-        for key_fp in self.data_forceplates_sync.keys():
-            self.data_forceplates_sync[key_fp] = self.data_forceplates_sync[key_fp][:min_len]
+            for key_fp in self.data_forceplates_sync.keys():
+                self.data_forceplates_sync[key_fp] = self.data_forceplates_sync[key_fp][:min_len]
 
-        # Start time to zero
-        self.data_loadsol_sync["time"] -= self.data_loadsol_sync["time"][0]
-        self.data_forceplates_sync["time"] -= self.data_forceplates_sync["time"][0]
+            # Start time to zero
+            self.data_loadsol_sync["time"] -= self.data_loadsol_sync["time"][0]
+            self.data_forceplates_sync["time"] -= self.data_forceplates_sync["time"][0]
 
     def export_csv(self,export_directory:str, file_name:str):
 
@@ -110,22 +110,23 @@ class TrialAnalysis:
         fig1, axs = plt.subplots(2)
         fig1.suptitle("Comparison FP / LS")
         axs[0].set_title("Left insole")
-        axs[0].plot(self.time_ls, self.l_f_heel, label="heel")
-        axs[0].plot(self.time_ls, self.l_f_medial, label="medial")
-        axs[0].plot(self.time_ls, self.l_f_lateral, label="lateral")
-        axs[0].plot(self.time_ls, self.l_f_total, label="total")
-        axs[0].plot(self.time_fp, self.fp1_x, "-.", label="Fx")
-        axs[0].plot(self.time_fp, self.fp1_y, "-.", label="Fy")
-        axs[0].plot(self.time_fp, self.fp1_z, "-.", label="Fz")
+
+        axs[0].plot(self.data_loadsol_sync["time"], self.data_loadsol_sync["f_heel_l"], label="heel")
+        axs[0].plot(self.data_loadsol_sync["time"], self.data_loadsol_sync["f_medial_l"], label="medial")
+        axs[0].plot(self.data_loadsol_sync["time"], self.data_loadsol_sync["f_lateral_l"], label="lateral")
+        axs[0].plot(self.data_loadsol_sync["time"], self.data_loadsol_sync["f_total_l"], label="total")
+        axs[0].plot(self.data_forceplates_sync["time"], self.data_forceplates_sync["fx1"], "-.", label="Fx")
+        axs[0].plot(self.data_forceplates_sync["time"], self.data_forceplates_sync["fy1"], "-.", label="Fy")
+        axs[0].plot(self.data_forceplates_sync["time"], self.data_forceplates_sync["fz1"], "-.", label="Fz")
 
         axs[1].set_title("Right insole")
-        axs[1].plot(self.time_ls, self.r_f_heel, label="heel")
-        axs[1].plot(self.time_ls, self.r_f_medial, label="medial")
-        axs[1].plot(self.time_ls, self.r_f_lateral, label="lateral")
-        axs[1].plot(self.time_ls, self.r_f_total, label="total")
-        axs[1].plot(self.time_fp, self.fp2_x, "-.", label="Fx")
-        axs[1].plot(self.time_fp, self.fp2_y, "-.", label="Fy")
-        axs[1].plot(self.time_fp, self.fp2_z, "-.", label="Fz")
+        axs[1].plot(self.data_loadsol_sync["time"], self.data_loadsol_sync["f_heel_r"], label="heel")
+        axs[1].plot(self.data_loadsol_sync["time"], self.data_loadsol_sync["f_medial_r"], label="medial")
+        axs[1].plot(self.data_loadsol_sync["time"], self.data_loadsol_sync["f_lateral_r"], label="lateral")
+        axs[1].plot(self.data_loadsol_sync["time"], self.data_loadsol_sync["f_total_r"], label="total")
+        axs[1].plot(self.data_forceplates_sync["time"], self.data_forceplates_sync["fx2"], "-.", label="Fx")
+        axs[1].plot(self.data_forceplates_sync["time"], self.data_forceplates_sync["fy2"], "-.", label="Fy")
+        axs[1].plot(self.data_forceplates_sync["time"], self.data_forceplates_sync["fz2"], "-.", label="Fz")
 
         axs[0].set_xlabel("Time (s)")
         axs[0].set_ylabel("Force (N)")
@@ -137,10 +138,104 @@ class TrialAnalysis:
         fig1.subplots_adjust(hspace=0.3)
         plt.show()
 
+    def compare_forces_imu(self):
+        fig1, axs = plt.subplots(3)
+        fig1.suptitle("Comparison LS forces / IMU left insole")
+        axs[0].set_title("Forces")
+        if self.data_state == "pre_processed":
+            axs[0].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["f_heel_l"], label="heel")
+            axs[0].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["f_medial_l"], label="medial")
+            axs[0].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["f_lateral_l"], label="lateral")
+            axs[0].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["f_total_l"], label="total")
+        elif self.data_state == "filtered":
+            axs[0].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["f_heel_l"], label="heel")
+            axs[0].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["f_medial_l"], label="medial")
+            axs[0].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["f_lateral_l"], label="lateral")
+            axs[0].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["f_total_l"], label="total")
+
+        axs[1].set_title("Accelerometer")
+        if self.data_state == "pre_processed":
+            axs[1].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["acc_x_l"], label="acc x")
+            axs[1].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["acc_y_l"], label="acc y")
+            axs[1].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["acc_z_l"], label="acc z")
+        elif self.data_state == "filtered":
+            axs[1].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["acc_x_l"], label="acc x")
+            axs[1].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["acc_y_l"], label="acc y")
+            axs[1].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["acc_z_l"], label="acc z")
+
+        axs[2].set_title("Gyrometer")
+        if self.data_state == "pre_processed":
+            axs[2].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["gyro_x_l"], label="gyro x")
+            axs[2].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["gyro_y_l"], label="gyro y")
+            axs[2].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["gyro_z_l"], label="gyro z")
+        elif self.data_state == "filtered":
+            axs[2].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["gyro_x_l"], label="gyro x")
+            axs[2].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["gyro_y_l"], label="gyro y")
+            axs[2].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["gyro_z_l"], label="gyro z")
+
+        axs[0].set_xlabel("Time (s)")
+        axs[0].set_ylabel("Force (N)")
+        axs[1].set_xlabel("Time (s)")
+        axs[1].set_ylabel("Acceleration (g)")
+        axs[2].set_xlabel("Time (s)")
+        axs[2].set_ylabel("Angular velocity (rad/s)")
+
+        axs[0].legend()
+        axs[1].legend()
+        axs[2].legend()
+        fig1.subplots_adjust(hspace=0.3)
+
+        fig2, axs = plt.subplots(3)
+        fig2.suptitle("Comparison LS forces / IMU right insole")
+        axs[0].set_title("Forces")
+        if self.data_state == "pre_processed":
+            axs[0].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["f_heel_r"], label="heel")
+            axs[0].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["f_medial_r"], label="medial")
+            axs[0].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["f_lateral_r"], label="lateral")
+            axs[0].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["f_total_r"], label="total")
+        elif self.data_state == "filtered":
+            axs[0].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["f_heel_r"], label="heel")
+            axs[0].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["f_medial_r"], label="medial")
+            axs[0].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["f_lateral_r"], label="lateral")
+            axs[0].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["f_total_r"], label="total")
+
+        axs[1].set_title("Accelerometer")
+        if self.data_state == "pre_processed":
+            axs[1].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["acc_x_r"], label="acc x")
+            axs[1].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["acc_y_r"], label="acc y")
+            axs[1].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["acc_z_r"], label="acc z")
+        elif self.data_state == "filtered":
+            axs[1].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["acc_x_r"], label="acc x")
+            axs[1].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["acc_y_r"], label="acc y")
+            axs[1].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["acc_z_r"], label="acc z")
+
+        axs[2].set_title("Gyrometer")
+        if self.data_state == "pre_processed":
+            axs[2].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["gyro_x_r"], label="gyro x")
+            axs[2].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["gyro_y_r"], label="gyro y")
+            axs[2].plot(self.data_loadsol.downsampled_data["time"], self.data_loadsol.downsampled_data["gyro_z_r"], label="gyro z")
+        elif self.data_state == "filtered":
+            axs[2].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["gyro_x_r"], label="gyro x")
+            axs[2].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["gyro_y_r"], label="gyro y")
+            axs[2].plot(self.data_loadsol.filtered_data["time"], self.data_loadsol.filtered_data["gyro_z_r"], label="gyro z")
+
+        axs[0].set_xlabel("Time (s)")
+        axs[0].set_ylabel("Force (N)")
+        axs[1].set_xlabel("Time (s)")
+        axs[1].set_ylabel("Acceleration (g)")
+        axs[2].set_xlabel("Time (s)")
+        axs[2].set_ylabel("Angular velocity (rad/s)")
+
+        axs[0].legend()
+        axs[1].legend()
+        axs[2].legend()
+        fig2.subplots_adjust(hspace=0.3)
+        plt.show()
+
 
 if __name__ == "__main__":
     curr_path = getcwd()
-    working_directory = curr_path + "\\tests_09_02_24\\data\\"
+    working_directory = curr_path + "\\tests_09_02_24\\data\\txt\\"
 
     # List of trial names
     files_names = listdir(working_directory)
@@ -171,6 +266,9 @@ if __name__ == "__main__":
         )
 
         Trial.export_csv(export_directory=curr_path + "\\tests_09_02_24\\results\\filtered\\",file_name=file_name)
+
+        Trial.compare_loadsol_forceplates()
+        Trial.compare_forces_imu()
 
     #     # Initialise data (synchronised + downsampled signals)
     #     plt.plot(Trial.data_loadsol_sync["time"],Trial.data_loadsol_sync['f_total_l'],"-o",label="f total left")
